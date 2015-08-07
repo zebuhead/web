374037010e47e0dcc3da5e72f7b1d9eacb98a7d1
unknown <johnpdees@berkeley.edu>
2015-08-07 12:39:18 -0400

    Final before web

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 
